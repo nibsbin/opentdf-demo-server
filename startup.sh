4dd5b763d72ff99897e79933c0a608cc768a21ac
Nibs <jpthek9@gmail.com>
2025-10-01 21:13:13 -0400

    Add sudo

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-./.github/scripts/init-temp-keys.sh
+sudo ./.github/scripts/init-temp-keys.sh
 sudo cp ./keys/localhost.crt /usr/local/share/ca-certificates/ && sudo update-ca-certificates
 
 # Kill existing containers and start fresh
