722aef228a4313128a305c6c6230ee09f89cae3d
Nibs <jpthek9@gmail.com>
2025-10-19 01:25:54 +0000

    Fix execution order

--- startup.sh
+++ startup.sh
@@ -3,9 +3,11 @@ sudo docker compose up -d --wait
 
 sleep 3
 
-sudo go run ./service provision keycloak && \
-sudo go run ./service provision fixtures && \
-sudo go run ./service start
+sudo go run ./service provision keycloak
+sudo go run ./service provision fixtures
+sudo go run ./service start &
+
+sleep 6
 
 # test
-otdfctl decrypt test_text.txt.tdf --host http://localhost:8080 --with-client-creds '{"clientId":"opentdf","clientSecret":"secret"}' --tls-no-verify -o test_decrypted.txt
+otdfctl policy attributes list --host http://localhost:8080 --with-client-creds '{"clientId":"opentdf","clientSecret":"secret"}' --tls-no-verify
