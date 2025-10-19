ce8343193ccd876e5de87d72b59cf2762de3dea8
Nibs <jpthek9@gmail.com>
2025-10-19 01:20:37 +0000

    Serialize commands and add otdfctl check to startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,9 @@ sudo docker compose up -d --wait
 
 sleep 3
 
-sudo go run ./service provision keycloak
-sudo go run ./service provision fixtures
+sudo go run ./service provision keycloak && \
+sudo go run ./service provision fixtures && \
 sudo go run ./service start
 
+# test
+otdfctl decrypt test_text.txt.tdf --host http://localhost:8080 --with-client-creds '{"clientId":"opentdf","clientSecret":"secret"}' --tls-no-verify -o test_decrypted.txt
